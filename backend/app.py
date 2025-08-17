8c37842f313b4ac63b4985250b0d82a1acdd16ac
Tea <tea.vkk@gmail.com>
2025-08-17 20:23:39 +0200

    add backend as regular folder

---
+++ backend/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from routes.simple_mix import simple_bp
+from routes.complex_mix import complex_bp
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Test routes
+@app.route('/')
+def home():
+    return "Backend is working!"
+
+@app.route('/test')
+def test():
+    return jsonify({"message": "API is working!"})
+
+# registracija blueprintova
+app.register_blueprint(simple_bp, url_prefix="/api/mix")
+app.register_blueprint(complex_bp, url_prefix="/api/mix")
+
+if __name__ == '__main__':
+    app.run(debug=True)
